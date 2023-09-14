985011e138aa028c0f83aeee87be13c6d6b3d0eb
Arman Aleqyan <manegevorgyan1@gmail.com>
2023-09-14 14:29:40 +0400

    carousel bug fix

--- src/components/popup/singleProduct/index.jsx
+++ src/components/popup/singleProduct/index.jsx
@@ -1,19 +1,9 @@
 import './style.css'
 import { CloseIcon } from '../../svg'
-import { useEffect, useState } from 'react'
-import { Carousel } from 'react-carousel-minimal'
+import { Carousel } from 'react-responsive-carousel'
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 export const SingleProduct = ({ open, setOpen, product }) => {
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        let photos = []
-        product?.product_image?.forEach(elm => {
-            photos.push({ image: `${process.env.REACT_APP_IMAGE}${elm.image}` })
-        })
-        setData(photos)
-    }, [product?.product_image])
-
     return (
         <div className={open ? 'activePopup' : 'inactive'}>
             <div className='pop' style={window.matchMedia("(max-width: 1240px)").matches ? { width: '670px' } : { width: '970px' }}>
@@ -22,12 +12,14 @@ export const SingleProduct = ({ open, setOpen, product }) => {
                 </div>
                 <div className='eachSingleProdDetails'>
                     <Carousel
-                        data={data}
-                        width={'100%'}
-                        height="500px"
-                        dots={true}
-                        slideImageFit="fill"
-                    />
+                        style={styles}
+                        showThumbs={false}
+                        showStatus={false}
+                    >
+                        {product?.product_image?.length > 0 && product?.product_image?.map((e, i) => (
+                            <img alt='' key={i} src={`${process.env.REACT_APP_IMAGE}${e.image}`} className='carouselImages' />
+                        ))}
+                    </Carousel>
                 </div>
                 <div className='eachSingleProdDetails'>
                     <div className='eachManuProduct'>
