487bb54370dff5a3b6b307e771bbbfc1dc6350c2
Arman Aleqyan <manegevorgyan1@gmail.com>
2023-09-29 09:50:21 +0400

    token check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { useEffect } from 'react'
 import { MyRouter } from './MyRouter'
+import { useDispatch } from 'react-redux'
+import { Logout } from './Redux/action/auth_action'
 
-function App() { return <MyRouter /> }
+function App() {
+    const token = localStorage.getItem('token')
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if(!token && window.location.pathname !== '/auth/login') {
+            dispatch(Logout())
+        }
+    }, [token, dispatch])
+
+    return <MyRouter />
+}
 
 export default App
